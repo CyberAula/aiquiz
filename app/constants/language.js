b0b29001e892782532fab30a8c6f75cf4993f0b7
javicond3 <javier.conde.diaz@upm.es>
2024-03-14 00:04:12 +0100

    add TECW

--- app/constants/language.js
+++ app/constants/language.js
@@ -10,6 +10,16 @@ export const language = {
     { value: 'javascript', label: 'Lenguaje JavaScript' },
     { value: 'node', label: 'Node' },
   ],
+  TECW: [
+    { value: 'http', label: 'HTTP' },
+    { value: 'html', label: 'HTML' },
+    { value: 'css', label: 'CSS' },
+    { value: 'javascript_cliente', label: 'JavaScript Cliente' },
+    { value: 'javascript', label: 'Lenguaje JavaScript' },
+    { value: 'node', label: 'Node' },
+    { value: 'sequelize', label: 'Sequelize' },
+    { value: 'express', label: 'Express'},
+  ],
   BBDD: [
     { value: 'intro_nosql', label: 'Introducción a NoSQL' },
     { value: 'json', label: 'JSON y JSON Schema' },
