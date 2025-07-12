5cde63ceb8be48987c3e56e8706178ecd5baf3d1
javicond3 <4b3javi@gmail.com>
2025-07-13 00:37:09 +0200

    update middleware

--- middleware.js
+++ middleware.js
@@ -1,4 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
+import urlJoin from 'url-join';
+
+// Import basePath from next.config.js
+const nextConfig = require('./next.config.js');
+const basePath = nextConfig.basePath || '';
+const port = process.env.PORT_SERVER || 3000;
+const host = process.env.HOST_SERVER || 'localhost';
 
 export const config = {
   matcher: ["/dashboard", "/dashboard/:subject", "/reports", "/reports/:subject", "/reports/:subject/pregreport"],
@@ -11,19 +18,16 @@ export function middleware(req) {
     const BASIC_AUTH_USER = process.env.BASIC_AUTH_USER;
     const BASIC_AUTH_PASSWORD = process.env.BASIC_AUTH_PASSWORD;
 
-    const isHttps = headers.get("x-forwarded-proto")?.split(",")[0] === "https";
-    const isLocalhost = req.headers.get("host")?.includes("localhost");
-
-    console.log("MIDDLEWARE INICIAL - currentEnv: ", currentEnv, "isHttps: ", isHttps, "isLocalhost: ", isLocalhost);
-    if (currentEnv === "production" && !isHttps && !isLocalhost) {
-      const newUrl = new URL(`http://${headers.get("host")}` || "");
-      newUrl.protocol = "https:";
-      return NextResponse.redirect(newUrl.href, 301);
+    // Check if basic auth credentials are configured
+    if (!BASIC_AUTH_USER || !BASIC_AUTH_PASSWORD) {
+        console.log("Basic auth credentials not configured, allowing access");
+        return NextResponse.next();
     }
 
     const basicAuth = req.headers.get("authorization");
     const url = req.nextUrl;
     console.log("basicAuth: ", basicAuth);
+    
     if (basicAuth) {
         const authValue = basicAuth.split(" ")[1];
         const [user, pwd] = atob(authValue).split(":");
@@ -32,12 +36,15 @@ export function middleware(req) {
         const validPassWord = BASIC_AUTH_PASSWORD;
 
         if (user === validUser && pwd === validPassWord) {
-        return NextResponse.next();
+            console.log("Valido")
+            return NextResponse.next();
         }
     }
 
-    url.pathname = "/api/auth";
+    const apiAuthPath = urlJoin(basePath, 'api/auth');
+    const newUrl = new URL(`http://${host}:${port}${apiAuthPath}`);
+    console.log(`Sanitized URL: ${newUrl}`);
+    return NextResponse.rewrite(newUrl);
 
-    return NextResponse.rewrite(url);
 }
 
